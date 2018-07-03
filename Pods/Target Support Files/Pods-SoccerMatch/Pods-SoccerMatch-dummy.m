7bfec6ecdbb8c12c6deb4fecd71ec82247d52a15
Unknown <hercilio.ortiz@acad.pucrs.br>
2018-07-03 08:48:26 -0300

    Pod do Firebase instalado

---
+++ Pods/Target Support Files/Pods-SoccerMatch/Pods-SoccerMatch-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SoccerMatch : NSObject
+@end
+@implementation PodsDummy_Pods_SoccerMatch
+@end
--- SoccerMatch/AppDelegate.swift
+++ SoccerMatch/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import CoreData
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -9,6 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
