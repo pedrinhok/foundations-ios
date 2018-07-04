81425dcf694bc5e7a8f57a51fb23a60c500a3e47
Pedro Kayser <pedro.kayser@10-41-48-186.0200.epoa.br.iosda.org>
2018-07-04 11:26:54 -0300

    match location

--- SoccerMatch/ViewController.swift
+++ SoccerMatch/ViewController.swift
@@ -1,7 +1,11 @@
 import UIKit
 import CoreData
 
+import MapKit
+
 class ViewController: UIViewController {
+
+    var locationManager = CLLocationManager()
 
     @IBOutlet weak var email: StandardTextField!
     @IBOutlet weak var password: StandardTextField!
@@ -9,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        locationManager.delegate = self
         email.delegate = self
         password.delegate = self
     }
@@ -16,12 +21,29 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
+        locationManager.requestWhenInUseAuthorization()
+
         if UserService.auth() {
             performSegue(withIdentifier: "gotoHome", sender: nil)
         }
     }
 
-    @IBAction func clickSignin(_ sender: StandardButton) {}
+    @IBAction func clickSignin(_ sender: StandardButton) {
+        locationManager.requestWhenInUseAuthorization()
+    }
+
+}
+
+extension ViewController: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            print("authorized")
+        default:
+            print("unauthorized")
+        }
+    }
 
 }
 
--- SoccerMatch/home/LocationViewController.swift
+++ SoccerMatch/home/LocationViewController.swift
@@ -1,7 +1,41 @@
 import UIKit
 
+import MapKit
+
 class LocationViewController: UIViewController {
 
     var match: Match!
+    var locationManager = CLLocationManager()
+    var geocoder = CLGeocoder()
+
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var address: StandardTextField!
+    @IBOutlet weak var name: StandardTextField!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        currentPosition()
+    }
+
+    func currentPosition() {
+        guard let location = locationManager.location else { return }
+
+        geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            if error == nil {
+                if let placemark = placemarks?.first {
+                    self.address.text = placemark.name
+                }
+            }
+        }
+
+        var region = MKCoordinateRegion()
+        region.center.latitude = location.coordinate.latitude
+        region.center.longitude = location.coordinate.longitude
+        region.span.longitudeDelta = 0.015
+
+        map.setRegion(region, animated: true)
+        map.showsUserLocation = true
+    }
 
 }
