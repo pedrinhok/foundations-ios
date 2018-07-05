c84de803043e0656b133bfb811d94983491aa22a
Camile Alves Ancines <camile.ancines@10-41-40-10.0101.epoa.br.iosda.org>
2018-07-05 09:59:16 -0300

    Profile iniciado

--- SoccerMatch/profile/ChangePasswordViewController.swift
+++ SoccerMatch/profile/ChangePasswordViewController.swift
@@ -1,3 +1,59 @@
 import UIKit
 
-class ChangePasswordViewController: UIViewController {}
+class ChangePasswordViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var password: StandardTextField!
+    @IBOutlet weak var newPassword: StandardTextField!
+    @IBOutlet weak var confirmPassword: StandardTextField!
+    
+    override func viewDidLoad() {
+        password.delegate = self
+        newPassword.delegate = self
+        confirmPassword.delegate = self
+        
+        password.isSecureTextEntry = true
+        newPassword.isSecureTextEntry = true
+        confirmPassword.isSecureTextEntry = true
+    }
+    
+    func showMessage(_ message: String) {
+        let alert = UIAlertController(title: "Wops", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default) { (action) in
+            alert.dismiss(animated: true)
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+    func showMessageNewPassword(_ message: String) {
+        let alert = UIAlertController(title: "Done", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default) { (action) in
+            alert.dismiss(animated: true)
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+    @IBAction func changePassword(_ sender: StandardButton) {
+        
+        guard let newPassword = newPassword.text, newPassword != "" else {
+            showMessage("Password cannnot be empty!")
+            return
+        }
+        if newPassword.count < 3 {
+            showMessage("Password must be at least 3 characters!")
+            return
+        }
+        guard let confirmPassword = confirmPassword.text, newPassword == confirmPassword else{
+            showMessage("Password does not match your confirmation!")
+            return
+        }
+        if confirmPassword == newPassword{
+            showMessageNewPassword("Password changed!")
+        }
+  
+    }
+
+}
--- SoccerMatch/profile/ProfileViewController.swift
+++ SoccerMatch/profile/ProfileViewController.swift
@@ -1,3 +1,124 @@
 import UIKit
+import CoreData
 
-class ProfileViewController: UIViewController {}
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate,  UITextFieldDelegate{
+    
+    
+    @IBOutlet weak var name: StandardTextField!
+    @IBOutlet weak var gender: StandardTextField!
+    @IBOutlet weak var birthday: StandardTextField!
+    @IBOutlet weak var phone: StandardTextField!
+    @IBOutlet weak var email: StandardTextField!
+    @IBOutlet weak var photo: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        //imagePicker.delegate = self 
+        name.delegate = self
+        gender.delegate = self
+        birthday.delegate = self
+        phone.delegate = self
+        email.delegate = self
+       
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        if textField == self.name{
+            textField.backgroundColor = UIColor.lightGray
+            return true
+        }else if textField == self.gender{
+            textField.backgroundColor = UIColor.lightGray
+            return true
+        }else if textField == self.birthday{
+            textField.backgroundColor = UIColor.lightGray
+            return true
+        }else if textField == self.phone{
+            textField.backgroundColor = UIColor.lightGray
+            return true
+        }else if textField == self.email{
+            textField.backgroundColor = UIColor.lightGray
+            return true
+        }
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == self.name{
+  
+        }else if textField == self.gender{
+
+        }else if textField == self.birthday{
+
+        }else if textField == self.phone{
+
+        }else if textField == self.email{
+
+        }
+   }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField == self.name{
+            textField.backgroundColor = UIColor.white
+            return true
+        }else if textField == self.gender{
+            textField.backgroundColor = UIColor.white
+            return true
+        }else if textField == self.birthday{
+            textField.backgroundColor = UIColor.white
+            return true
+        }else if textField == self.phone{
+            textField.backgroundColor = UIColor.white
+            return true
+        }else if textField == self.email{
+            textField.backgroundColor = UIColor.white
+            return true
+        }
+        return false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == self.name{
+            self.name = textField as! StandardTextField
+        }else if textField == self.gender{
+            self.gender = textField as! StandardTextField
+        }else if textField == self.birthday{
+            self.birthday = textField as! StandardTextField
+        }else if textField == self.phone{
+            self.phone = textField as! StandardTextField
+        }else if textField == self.email{
+            self.email = textField as! StandardTextField
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func password(_ sender: UIButton) {
+         performSegue(withIdentifier: "gotoChangePassword", sender: nil)
+    }
+    
+    @IBAction func changePhoto(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage{
+            photo.image = image
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func clickSubmit(_ sender: StandardButton) {
+        
+        
+    }
+    
+    
+    
+}
+
