a18e0046d38288e4a53aa550611b9b61946bdcbb
Pedro Kayser <pedro.kayser@10-41-48-186.0200.epoa.br.iosda.org>
2018-07-05 14:30:02 -0300

    merge

--- SoccerMatch/home/HomeViewController.swift
+++ SoccerMatch/home/HomeViewController.swift
@@ -1,68 +1,28 @@
-//tirando os comentários já tem dois pontos de futebol em Poa ali. Tinha colocado também o framework //para
-//mapas e a solicitação de autorização do usuário para localização, mas isso é em outra parte do xcode
-
 import UIKit
+
 import MapKit
 
-
-class HomeViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
+class HomeViewController: UIViewController {
     
+    var locationManager = CLLocationManager()
+    var geocoder = CLGeocoder()
     
+    @IBOutlet weak var map: MKMapView!
     
-    @IBOutlet weak var mapas: MKMapView!
-    // @IBOutlet var mapas: MKMapView!
-    var gerenciadorLocal = CLLocationManager()
-    
-    //-30.000728, -51.191491 A BOMBONERA AV.CEARA
-    //-29.996852, -51.194481 HD SPORT CENTER RUA LAURO MULLER
-    //Rua Dona Margarida -29.999477, -51.193902
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        //Responsável por gerenciar objeto gerenciador é a própria classe
-        gerenciadorLocal.delegate = self
-        gerenciadorLocal.desiredAccuracy = kCLLocationAccuracyBest
-        gerenciadorLocal.requestWhenInUseAuthorization()
-        gerenciadorLocal.startUpdatingLocation()
-        
-        
-        
-                let latitude: CLLocationDegrees = -29.999477
-                let longitude: CLLocationDegrees = -51.193902
-        
-        
-                let deltaLatitude: CLLocationDegrees = 0.02
-                let deltaLongitude: CLLocationDegrees = 0.02
-        
-                let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
-        
-                let areaVisualizacao: MKCoordinateSpan = MKCoordinateSpanMake(deltaLatitude, deltaLongitude)
-        
-                let regiao: MKCoordinateRegion = MKCoordinateRegionMake(localizacao,areaVisualizacao)
-                mapas.setRegion(regiao, animated: true)
-        
-        
-                let quadra1 = MKPointAnnotation()
-                quadra1.coordinate.latitude = -30.000728
-                quadra1.coordinate.longitude = -51.191491
-                quadra1.title = "A BOMBONERA"
-                quadra1.subtitle = "Uma grande quadra"
-        
-                let quadra2 = MKPointAnnotation()
-                quadra2.coordinate.latitude = -29.996852
-                quadra2.coordinate.longitude = -51.194481
-                quadra2.title = "HD SPORT"
-                quadra2.subtitle = "Ao lado do trensurb!"
-        
-                mapas.addAnnotation(quadra1)
-                mapas.addAnnotation(quadra2)
-        
-        
-        
+        construct()
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print(locations)
+    func construct() {
+        locationManager.startUpdatingLocation()
+        guard let location = locationManager.location else { return }
+
+        let span = MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
+        let region = MKCoordinateRegion(center: location.coordinate, span: span)
+        map.setRegion(region, animated: true)
+        map.showsUserLocation = true
     }
     
     @IBAction func unwindHome(segue: UIStoryboardSegue) {}
@@ -72,5 +32,3 @@ class HomeViewController: UIViewController, MKMapViewDelegate, CLLocationManager
     }
 
 }
-
-
