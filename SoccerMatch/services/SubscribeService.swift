c04b17bd4bddef51a7b0bda49327ee07a8b5725a
Unknown <hercilio.ortiz@acad.pucrs.br>
2018-07-06 11:28:44 -0300

    Create subscribe complete

---
+++ SoccerMatch/services/SubscribeService.swift
@@ -0,0 +1,13 @@
+//
+//  SubscribeService.swift
+//  SoccerMatch
+//
+//  Created by Hercílio Martins Ortiz on 06/07/2018.
+//  Copyright © 2018 Pedro Kayser. All rights reserved.
+//
+
+import UIKit
+
+class SubscribeService: Any {
+
+}
