ca23071cdddfd045abd4f89abd7c584fa777d2f5
Unknown <hercilio.ortiz@acad.pucrs.br>
2018-07-11 15:09:03 -0300

    Loading adicionado na tela de signup

--- SoccerMatch/SignupViewController.swift
+++ SoccerMatch/SignupViewController.swift
@@ -3,6 +3,8 @@ import CoreData
 
 class SignupViewController: UIViewController {
 
+    @IBOutlet weak var viewLoading: UIView!
+    @IBOutlet weak var activityLoading: UIActivityIndicatorView!
     @IBOutlet weak var name: StandardTextField!
     @IBOutlet weak var phone: StandardTextField!
     @IBOutlet weak var email: StandardTextField!
@@ -20,6 +22,7 @@ class SignupViewController: UIViewController {
     }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         password.delegate = self
         confirmPassword.delegate = self
         
@@ -60,38 +63,49 @@ class SignupViewController: UIViewController {
 
     @IBAction func clickSignup(_ sender: StandardButton) {
 
+        showLoading()
+        hideKeyboard()
+        
         guard let name = name.text, name != "" else {
             showMessage("Name cannnot be empty!")
+            hideLoading()
             return
         }
         if name.count < 3 {
             showMessage("Name must be at least 6 characters!")
+            hideLoading()
             return
         }
         guard let phone = phone.text, phone != "" else {
             showMessage("Phone cannnot be empty!")
+            hideLoading()
             return
         }
         guard let email = email.text, email != "" else {
             showMessage("E-mail cannnot be empty!")
+            hideLoading()
             return
         }
         guard let password = password.text, password != "" else {
             showMessage("Password cannnot be empty!")
+            hideLoading()
             return
         }
         if password.count < 6 {
             showMessage("Password must be at least 6 characters!")
+            hideLoading()
             return
         }
         guard let confirmPassword = confirmPassword.text, password == confirmPassword else {
             showMessage("Password does not match your confirmation!")
+            hideLoading()
             return
         }
 
         UserService.create(name: name, phone: phone, email: email, password: password) { (error) in
             if let error = error {
                 self.showMessage(error)
+                self.hideLoading()
             } else {
                 self.performSegue(withIdentifier: "gotoHome", sender: nil)
             }
@@ -108,7 +122,18 @@ class SignupViewController: UIViewController {
 
         present(alert, animated: true)
     }
-
+    
+    func showLoading() {
+        viewLoading.isHidden = false
+        viewLoading.isUserInteractionEnabled = true
+        activityLoading.startAnimating()
+    }
+    
+    func hideLoading() {
+        viewLoading.isHidden = true
+        viewLoading.isUserInteractionEnabled = false
+        activityLoading.stopAnimating()
+    }
 }
 
 extension SignupViewController: UITextFieldDelegate {
