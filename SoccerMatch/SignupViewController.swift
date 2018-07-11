39b92c5c9f9e5e7959e83da609f820d42d8da400
Pedro Kayser <pedro.kayser@10-41-40-27.0101.epoa.br.iosda.org>
2018-07-11 14:35:07 -0300

    merge

--- SoccerMatch/SignupViewController.swift
+++ SoccerMatch/SignupViewController.swift
@@ -2,31 +2,32 @@ import UIKit
 import CoreData
 
 class SignupViewController: UIViewController {
-
+    
     @IBOutlet weak var name: StandardTextField!
     @IBOutlet weak var phone: StandardTextField!
     @IBOutlet weak var email: StandardTextField!
     @IBOutlet weak var password: StandardTextField!
     @IBOutlet weak var confirmPassword: StandardTextField!
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        password.delegate = self
+        confirmPassword.delegate = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
+        
         name.delegate = self
         phone.delegate = self
         email.delegate = self
         password.delegate = self
         confirmPassword.delegate = self
-    }
-    
-    override func viewDidLoad() {
-        password.delegate = self
-        confirmPassword.delegate = self
-        
-        //Listen for keyboard events
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
     }
     
     deinit {
@@ -35,31 +36,8 @@ class SignupViewController: UIViewController {
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
     }
     
-    func hideKeyboard(){
-        name.resignFirstResponder()
-        phone.resignFirstResponder()
-        email.resignFirstResponder()
-        password.resignFirstResponder()
-        confirmPassword.resignFirstResponder()
-    }
-    
-    @objc func keyboardWillChange(notification: Notification){
-        print("Keyboard will show:  \(notification.name.rawValue)")
-        guard let keyboardRect = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey]as? NSValue)?.cgRectValue else{
-            return
-        }
-        if notification.name == Notification.Name.UIKeyboardWillShow ||
-            notification.name == Notification.Name.UIKeyboardWillChangeFrame{
-            
-            view.frame.origin.y  = -keyboardRect.height
-        }else {
-            view.frame.origin.y = 0
-        }
-        
-    }
-
     @IBAction func clickSignup(_ sender: StandardButton) {
-
+        
         guard let name = name.text, name != "" else {
             showMessage("Name cannnot be empty!")
             return
@@ -88,7 +66,7 @@ class SignupViewController: UIViewController {
             showMessage("Password does not match your confirmation!")
             return
         }
-
+        
         UserService.create(name: name, phone: phone, email: email, password: password) { (error) in
             if let error = error {
                 self.showMessage(error)
@@ -97,27 +75,38 @@ class SignupViewController: UIViewController {
             }
         }
     }
-
+    
     func showMessage(_ message: String) {
         let alert = UIAlertController(title: "Wops", message: message, preferredStyle: .alert)
-
+        
         let action = UIAlertAction(title: "OK", style: .default) { (action) in
             alert.dismiss(animated: true)
         }
         alert.addAction(action)
-
+        
         present(alert, animated: true)
     }
-
+    
+    @objc func keyboardWillChange(notification: Notification){
+        guard let keyboardRect = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        if notification.name == Notification.Name.UIKeyboardWillShow || notification.name == Notification.Name.UIKeyboardWillChangeFrame {
+            view.frame.origin.y = -keyboardRect.height
+        } else {
+            view.frame.origin.y = 0
+        }
+    }
+    
 }
 
 extension SignupViewController: UITextFieldDelegate {
-
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-
+        
         textField.resignFirstResponder()
-
+        
         return true
     }
-
+    
 }
